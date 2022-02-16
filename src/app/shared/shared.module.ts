cfa8858613e707d9666206f53da624713b499abf
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2022-02-17 00:01:53 +0900

    基本となるコンポーネントの作成

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, SidenavComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
